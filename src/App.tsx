8b7008af87c434611fd2b107d0595e37fb03512c
AshishGowtham <122021623+AshishGowtham@users.noreply.github.com>
2024-03-13 11:06:03 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Emora from "./Emora";
+
+
+export default function App(): JSX.Element {
+  
+
+  return (
+    <>
+      <div style={{ display: "flex" }}>
+        <Emora />
+      </div>
+    </>
+  );
+}
---
+++ src/Emora.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+import { ReactMultiCrop, IOutputData } from "@berviantoleo/react-multi-crop";
+
+export default function Default(): JSX.Element {
+  const [cropValue, setCropValue] = React.useState<Array<IOutputData>>([]);
+
+  return (
+    <Grid container>
+      <Grid item xs>
+        <ReactMultiCrop
+          id="canvas"
+          input={{
+            onChange: function (value: Array<IOutputData>) {
+              setCropValue(value);
+            },
+          }}
+          image="https://picsum.photos/800"
+          record={{
+            clippings: [
+              { id: '1', rect: { x1: 0.0, y1: 0.0, x2: 0.2, y2: 0.2 }, rectPx: {} },
+            ],
+          }}
+          
+        />
+      </Grid>
+      <Grid item xs>
+        <h3>React Multi Crop</h3>
+        <h4>By Bervianto Leo P - 2021</h4>
+        <h5>Crop Result</h5>
+        {cropValue &&
+          cropValue.map((objectData: IOutputData, i) => {
+            const canvasElement = objectData?.canvasElement?.toDataURL();
+            return (
+              <div key={`crop-result-${i}`}>
+                Result {i}:
+                <img src={canvasElement} alt={`crop-${i}`} />
+              </div>
+            );
+          })}
+      </Grid>
+    </Grid>
+  );
+}
